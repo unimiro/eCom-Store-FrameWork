d4a13f9f6db81d379a34e842103eecdd0546e198
unimiro <macantolak@gmail.com>
2023-09-07 19:19:26 +0200

    Routing to home/shop:id item

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -19,6 +19,7 @@ import { ShopModule } from './shop/shop.module';
     HttpClientModule,
     CoreModule,
     ShopModule,
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
