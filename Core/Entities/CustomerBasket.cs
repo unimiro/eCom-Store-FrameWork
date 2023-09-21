e7429d5b4629f2253933ae91b70f1ec5193e8dc2
unimiro <macantolak@gmail.com>
2023-09-21 19:55:20 +0200

    Added basket backend

---
+++ Core/Entities/CustomerBasket.cs
@@ -0,0 +1,15 @@
+namespace Core.Entities
+{
+    public class CustomerBasket
+    {
+        public CustomerBasket(string id)
+        {
+            Id = id;
+        }
+
+        public CustomerBasket(){}
+
+        public string Id { get; set; }
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+    }
+}
