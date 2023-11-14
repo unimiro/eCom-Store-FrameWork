d402983a8ab061df5fecd51c5fb7e574abc3f19e
unimiro <macantolak@gmail.com>
2023-11-14 11:50:53 +0100

    Checkout + stepper

--- client/src/app/checkout/checkout-routing.module.ts
+++ client/src/app/checkout/checkout-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CheckoutComponent } from './checkout.component';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckoutSuccesComponent } from './checkout-succes/checkout-succes.component';
 
 const routes: Routes = [
-  {path: '', component: CheckoutComponent}
+  {path: '', component: CheckoutComponent},
+  {path: 'succes', component: CheckoutSuccesComponent},
 ]
 
 @NgModule({
---
+++ client/src/app/checkout/checkout-succes/checkout-succes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-succes',
+  templateUrl: './checkout-succes.component.html',
+  styleUrls: ['./checkout-succes.component.scss']
+})
+export class CheckoutSuccesComponent {
+
+}
